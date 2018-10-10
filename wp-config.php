e563504ae8f36810475409ec77428a2abb15781d
dmvo4935 <dmytro.voznenko@evry.com>
2018-10-10 13:29:02 +0300

    added appspec.yml file

--- wp-config.php
+++ wp-config.php
@@ -20,16 +20,16 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', 'martin');
+define('DB_NAME', 'wordpress');
 
 /** MySQL database username */
-define('DB_USER', 'martin@seecure-mysql-db');
+define('DB_USER', 'wordpress');
 
 /** MySQL database password */
-define('DB_PASSWORD', '=271}gc@C_HTId]');
+define('DB_PASSWORD', 'PAssw0rd');
 
 /** MySQL hostname */
-define('DB_HOST', 'seecure-mysql-db.mysql.database.azure.com');
+define('DB_HOST', 'wordpresstest.cqqms8whpyl4.eu-west-1.rds.amazonaws.com');
 
 /** Database Charset to use in creating database tables. */
 define('DB_CHARSET', 'utf8');
